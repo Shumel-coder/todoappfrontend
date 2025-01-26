100b427ba9b50dd3053abd251d4ddf522f52e34b
Muhaimin Ali <muhaiminali@ymail.com>
2025-01-26 14:56:28 -0500

    added node version to json

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ const App: React.FC = () => {
   // Fetch Todos
   useEffect(() => {
     fetchTodos();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchFilter, user]);
 
   const fetchTodos = async () => {
